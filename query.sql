b49006efb2eece6887f113be18911f50f233275d
espoirMur <espoir.mur@gmail.com>
2018-11-26 23:00:04 +0200

    [ch #[162218388]] inittialization of database integration

--- query.sql
+++ query.sql
@@ -1,26 +1,30 @@
-`CREATE TABLE users(
-  id serial PRIMARY KEY,
-  name VARCHAR (50) UNIQUE NOT NULL,
-  passwordHash VARCHAR (500) NOT NULL,
-  email VARCHAR (55) UNIQUE NOT NULL,
-  registrationDate TIMESTAMP NOT NULL,
-  phone VARCHAR (50) NOT NULL,
-  isAdmin boolean not NULL
- );
 
- CREATE TABLE orders(
-  id serial PRIMARY KEY,
-  origin VARCHAR (50) NOT NULL,
-  destination VARCHAR (50) NOT NULL,
-  presentLocation VARCHAR (50) ,
-  recipientPhone VARCHAR (35) NULL,
-  orderDate TIMESTAMP NOT NULL,
-  deliveryDate TIMESTAMP,
-  comments VARCHAR (350) ,
-  status VARCHAR(50) NOT NULL,
-  initiatorId INTEGER,
-  weight INTEGER,
-  CONSTRAINT order_initiator_id_fk FOREIGN KEY (initiatorId)
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    name character varying(50) NOT NULL,
+    passwordhash character varying(500) NOT NULL,
+    email character varying(55) UNIQUE NOT NULL,
+    registrationdate timestamp without time zone DEFAULT now(),
+    phone character varying(50) NOT NULL,
+    isadmin boolean NOT NULL
+);
+
+
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    origin character varying(50) NOT NULL,
+    destination character varying(50) NOT NULL,
+    presentlocation character varying(50),
+    recipientphone character varying(35) NOT NULL,
+    orderdate timestamp without time zone DEFAULT now(),
+    deliverydate timestamp without time zone,
+    comments character varying(350),
+    status character varying(50) NOT NULL,
+    initiatorid integer,
+    weight integer
+);
+
+CONSTRAINT order_initiator_id_fk FOREIGN KEY (initiatorId)
        REFERENCES users (id) MATCH SIMPLE
        ON UPDATE NO ACTION ON DELETE NO ACTION
- );`;
+ );
