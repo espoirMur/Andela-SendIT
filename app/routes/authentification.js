c4c6870900a40b275fe512c48b4a40aa44e0b249
espoirMur <espoir.mur@gmail.com>
2018-11-27 10:09:22 +0200

    [ch #[162218388]] fix test on duplicate email and add logging info

--- app/routes/authentification.js
+++ app/routes/authentification.js
@@ -32,7 +32,7 @@ authRouter.post(
       .catch((error) => {
         // if an error check if it's related to duplicated user
         console.log(error, '=================');
-        if (error.code === '23505' && error.constraint === 'users_email_key') {
+        if (error.code === '23505') {
           // 23505 means duplicate key entry and constraint is to show if the email is taken
           // status 409 duplicate data
           return res.status(409).send({
