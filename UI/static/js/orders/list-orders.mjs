6595bb3c7a6eba94d3e531a95c66052fbc52ee06
Espoir Murhabazi <espoir.mur@gmail.com>
2018-12-18 16:39:35 +0200

    [ft #[162234274]] Fetch list user order api and create order table dynamically

--- UI/static/js/orders/list-orders.mjs
+++ UI/static/js/orders/list-orders.mjs
@@ -9,11 +9,28 @@ const getOrders = async () => {
   const url = `/api/v1/users/${user.id}/parcels`;
 
   fetchUrl(url, 'GET', undefined, token)
-    .then(async (successData) => {
-      console.log(successData);
+    .then(async (data) => {
+      if (!data.orders.length) {
+        _('my-orders-tbl').style.display = 'none';
+      } else {
+        data.orders.forEach((order) => {
+          _('my-orders-tbl-body').innerHTML += ` <tr data-id=${order.id}>
+        <td data-label="Order Id">${order.id}</td>
+        <td data-label="Date">${order.orderdate.split('T')[0]}</td>
+        <td data-label="Price">${order.price || '0'}</td>
+        <td data-label="Origin"> ${order.origin} </td>
+        <td data-label="Location"> ${order.presentlocation ||
+          order.origin} </td>
+        <td data-label="Destination"> ${order.destination}</td>
+        <td data-label="Tracking"> ${order.trackingnumber || '...'}</td>
+        <td data-label="Status"> ${order.status || '...'} </td>
+        <td data-label="Action"><a class="edit-order">Edit</a> <a class="delete-order">Cancel</a> </td>
+      </tr>`;
+        });
+      }
     })
-    .catch((errorData) => {
-      console.log(errorData);
+    .catch((data) => {
+      console.log(data);
     });
 };
 
